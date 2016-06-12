894e52bdfa7262455d17d56ea3dc9f769a22e1bf
Federico Giraud <giraud.federico@gmail.com>
2016-06-12 11:30:28 +0100

    Covering the stains - Difficult - 80
    Still too slow / too memory

--- algorithms/dynamic_programming/covering-the-stains/src/brute_force.rs
+++ algorithms/dynamic_programming/covering-the-stains/src/brute_force.rs
@@ -1,3 +1,4 @@
+use std::collections::btree_map::BTreeMap;
 use std::cmp::max;
 use std::cmp::min;
 use std::io::prelude::*;
@@ -7,7 +8,6 @@ use std::borrow::Borrow;
 
 const MIN_S: i32 = 0;
 const MAX_S: i32 = 100000;
-const MOD: i64 = 1000000007;
 
 #[derive(Debug, Eq, PartialEq)]
 struct Stain(i32, i32, Cell<bool>);
@@ -86,7 +86,7 @@ fn take_n<T>(v: &[T], n: i32) -> Vec<Vec<&T>> {
 
     let mut res = Vec::new();
     res.extend(take_n(&v[1..], n).iter().cloned());
-    for c in take_n(&v[1..], n - 1) {
+    for c in take_n(&v[1..], n-1) {
         let mut tmp = c.clone();
         tmp.push(&v[0]);
         res.push(tmp);
@@ -95,74 +95,22 @@ fn take_n<T>(v: &[T], n: i32) -> Vec<Vec<&T>> {
     res
 }
 
-fn bin_coeff_(n: i64, k: i64) -> i64 {
-    if k < 0 || k > n {
-        return 0;
-    }
-    if k == 0 || k == n {
-        return 1;
-    }
-    let k = min(k, n - k);
-    let mut res = 1;
-    for i in 0..k {
-        res = res * (n - i) / (i + 1);
-    }
-    res
-}
-
-// From: http://stackoverflow.com/a/24500377/1025899
-fn bin_coeff(n: i64, k: i64, m: i64) -> i64 {
-    let mut coef = 1;
-    let mut sieve = vec![false; (n+1) as usize];
-
-    for p in 2..(n + 1) {
-        if sieve[p as usize] {
-            continue;
-        }
-        let mut i = p * p;
-        while i <= n {
-            sieve[i as usize] = true;
-            i += p;
-        }
-        let mut pow = p;
-        while pow <= n {
-            let cnt = n / pow - k / pow - (n - k) / pow;
-            for _ in 0..cnt {
-                coef *= p;
-                coef %= m;
-            }
-            pow *= p;
-        }
-    }
-    coef
-}
-
 fn main() {
     let (clean, stains) = read_input();
     let borders = Borders::from_stains(&stains);
 
     let mut res = 0;
-    let rim = stains.iter()
-        .filter(|s| {
-            s.0 == borders.startx || s.0 == borders.endx || s.1 == borders.starty ||
-            s.1 == borders.endy
-        })
-        .collect::<Vec<_>>();
 
-    for comb in take_n(&rim, clean) {
-        if comb.len() == 0 {
+    for comb in take_n(&stains, clean) {
+        if comb.len() as i32 != clean {
             continue;
         }
         for stain in &comb {
             stain.2.set(true);
         }
         if Borders::from_stains(&stains) != borders {
-            let still_to_clean = clean as i64 - comb.len() as i64;
-            let internal_stains = (stains.len() - rim.len()) as i64;
-            if internal_stains >= still_to_clean {
-                res += bin_coeff(internal_stains, still_to_clean, MOD);
-            }
-            res %= MOD;
+            // println!("> {:?}", comb);
+            res += 1;
         }
         for stain in &comb {
             stain.2.set(false);
