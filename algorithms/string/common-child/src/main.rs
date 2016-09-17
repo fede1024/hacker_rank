d31c8a3ed7147928f683b0cd14879b6a57b48d59
Federico Giraud <giraud.federico@gmail.com>
2016-09-17 12:29:23 +0100

    No optimizations

---
+++ algorithms/string/common-child/src/main.rs
@@ -0,0 +1,65 @@
+use std::collections::HashMap;
+use std::io::prelude::*;
+use std::io;
+
+fn read_input() -> (Vec<u8>, Vec<u8>) {
+    let stdin = io::stdin();
+    let content = stdin.lock()
+        .lines()
+        .map(|line| {
+            line.expect("Input not readable").as_bytes().to_vec()
+        })
+        .collect::<Vec<_>>();
+    (content[0].clone(), content[1].clone())
+}
+
+type Chain = Vec<Option<usize>>;
+
+fn char_chain(s: &Vec<u8>, ref_char: u8) -> Chain {
+    let mut last_pos = None;
+    let mut chain = vec![None; s.len()];
+    for i in (0..s.len()).rev() {
+        if s[i] == ref_char {
+            last_pos = Some(i);
+        }
+        chain[i] = last_pos;
+    }
+    chain
+}
+
+fn find_common(a: &Vec<u8>, chains_b: &HashMap<u8, Chain>, pos_a: usize, pos_b: usize, len: usize) {
+    if pos_a >= a.len() {
+        return;
+    }
+
+    // for _ in (0..len) {
+    //     print!("  ");
+    // }
+    // println!("{} {}   {}", pos_a, pos_b, len);
+
+    if let Some(next_b) = chains_b.get(&a[pos_a]).unwrap()[pos_b] {
+        // for _ in (0..len) {
+        //     print!("  ");
+        // }
+        // println!("found {} {}   {}", pos_a, pos_b, next_b);
+        find_common(a, chains_b, pos_a + 1, next_b + 1, len + 1);
+    }
+
+    find_common(a, chains_b, pos_a + 1, pos_b, len);
+}
+
+fn main() {
+    let (str_a, str_b) = read_input();
+    let chains: HashMap<u8, Chain> = HashMap::new();
+
+    let chains = (65..91)
+        .map(|c| (c, char_chain(&str_b, c)))
+        .collect::<HashMap<_, _>>();
+
+    for (&c, chain) in &chains {
+        println!("{} {:?}", c as char, chain);
+    }
+
+    find_common(&str_a, &chains, 0, 0, 0);
+
+}
